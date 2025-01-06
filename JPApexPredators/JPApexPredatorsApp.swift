024ea037f22942d50f136f59fe2573528a9c1df4
waizin28 <waizinlinn.08@gmail.com>
2025-01-06 16:49:52 -0600

    Initial Commit

---
+++ JPApexPredators/JPApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredatorsApp.swift
+//  JPApexPredators
+//
+//  Created by Wai Zin Linn on 1/6/25.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
