7f4654bc0fd93fb3ce6a9aaba7bbd991e0fe23c2
waizin28 <waizinlinn.08@gmail.com>
2025-01-10 11:53:24 -0600

    added descriptions for predator details

--- JPApexPredators/ApexPredator.swift
+++ JPApexPredators/ApexPredator.swift
@@ -21,7 +21,7 @@ struct ApexPredator: Decodable, Identifiable { //Decodable protocol json to swif
         name.lowercased().replacingOccurrences(of: " ", with: "")
     }
     
-    struct MovieScene: Decodable {
+    struct MovieScene: Decodable, Identifiable {
         let id: Int
         let movie: String
         let sceneDescription: String
--- JPApexPredators/PredatorDetail.swift
+++ JPApexPredators/PredatorDetail.swift
@@ -27,20 +27,51 @@ struct PredatorDetail: View {
                         .scaledToFit()
                         .frame(width: geo.size.width/1.5, height: geo.size.height/3.7) // reszing based on device size
                         .scaleEffect(x: -1) //flip the image horizontally
-                        .shadow(color: .black, radius: 7) //make image 3d like
+                        .shadow(color: .black, radius: 7) //makeimage 3d like
                         .offset(y: 20) // move image down
                     
                 }
                 
-                // Name of Dino
-                
-                // Current Location
-                
-                // List of movies dino appear in
-                
-                // Movie moment
-                
-                // Link to webpage
+                VStack(alignment: .leading){
+                    // Name of Dino
+                    Text(predator.name)
+                        .font(.largeTitle)
+                    
+                    // Current Location
+                    
+                    // List of movies dino appear in
+                    Text("Appear In:")
+                        .font(.title3)
+                    
+                    ForEach(predator.movies, id: \.self){
+                        movie in
+                        Text("•" + movie)
+                            .font(.subheadline)
+                    }
+                    
+                    // Movie moment
+                    Text("Movie Moments")
+                        .font(.title)
+                        .padding(.top, 15)
+                    
+                    ForEach(predator.movieScenes){ scene in
+                        Text(scene.movie)
+                            .font(.title2)
+                            .padding(.vertical, 1)
+                        
+                        Text(scene.sceneDescription)
+                            .padding(.bottom, 15)
+                    }
+                    
+                    // Link to webpage
+                    Text("Read More:")
+                        .font(.caption)
+                    
+                    Link(predator.link, destination: URL(string: predator.link)!)
+                        .font(.caption)
+                        .foregroundStyle(.blue)
+                    
+                }.padding().padding(.bottom).frame(width: geo.size.width, alignment: .leading) // to add alignment to the vstack itself, otherwise will be at center
             }
         }.ignoresSafeArea()
     }
